291e2570458614185f3f7fe0fd1a5e2aa387a7e5
Yokai134 <118306934+Yokai134@users.noreply.github.com>
2023-03-27 14:44:53 +0500

    Update RegisterController.js
    users => Users

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -7,7 +7,7 @@ exports.Registr = (req,res)=>{
             response.statusError(rows,res,400);
         }
         else{
-            db.query(`INSERT INTO bh8pzn9jfci7qpzb9jo2.users (Email,Password) VALUES ('${login}', '${pwd}');`, (error,rows,fields) =>{
+            db.query(`INSERT INTO bh8pzn9jfci7qpzb9jo2.Users (Email,Password) VALUES ('${login}', '${pwd}');`, (error,rows,fields) =>{
                 response.statuslog(201,res);
         })
         }
