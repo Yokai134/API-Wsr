9d1b997ebebb950f7eaf72469f7da1f12076921f
Yokai134 <118306934+Yokai134@users.noreply.github.com>
2023-03-27 14:45:26 +0500

    Update UserController.js
    users => Users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,7 +3,7 @@ const db = require('../settings/config')
 
 exports.users = (req,res)=>{
 
-db.query('SELECT * FROM bh8pzn9jfci7qpzb9jo2.users', (error,rows,fields) =>{
+db.query('SELECT * FROM bh8pzn9jfci7qpzb9jo2.Users', (error,rows,fields) =>{
     if(error){
         response.statusError(rows,res,400);
     }else{
